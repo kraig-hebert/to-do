bf7fe3bdca04d4ae1b1ef575b362ca806be159b8
Kraig Hebert <kraighebert@gmail.com>
2022-08-01 06:55:55 -0400

    deleted unecessary import statement

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -3,7 +3,6 @@ import React from 'react';
 import { Box } from '@mui/material';
 import { DataGrid } from '@mui/x-data-grid';
 import * as SX from './mainSettings';
-import { getColumns } from './mainSettings';
 
 import { useStateContext } from '../../contexts/ContextProvider';
 
@@ -16,7 +15,7 @@ const Main = () => {
         sx={SX.dataGridSX}
         rows={mainTextList}
         rowHeight={75}
-        columns={getColumns(handleStatusUpdate)}
+        columns={SX.getColumns(handleStatusUpdate)}
         pageSize={15}
         rowsPerPageOptions={[15]}
         disableSelectionOnClick
